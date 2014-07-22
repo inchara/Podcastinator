730105892f3e5e0a629222260411a41a0051f1cc
Abhik11 <abhikchatterjee501@gmail.com>
2014-07-22 13:44:08 -0700

    againtryingagaintrying

---
+++ HTMLAgilityPack/HTMLAgilityPack/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+using System.IO;
+using System.Net;
+                           
+
+namespace HTMLAgilityPack
+{
+    public class HtmlToText
+    {
+        #region Public Methods
+
+        public string Convert(string path)
+        {
+            WebClient wc = new WebClient();
+            HtmlDocument doc = new HtmlDocument();
+            doc.Load(wc.OpenRead(path));
+
+            StringWriter sw = new StringWriter();
+            ConvertTo(doc.DocumentNode, sw);
+            sw.Flush();
+            return sw.ToString();
+        }
+
+        public string ConvertHtml(string html)
+        {
+            HtmlDocument doc = new HtmlDocument();
+            doc.LoadHtml(html);
+
+            StringWriter sw = new StringWriter();
+            ConvertTo(doc.DocumentNode, sw);
+            sw.Flush();
+            return sw.ToString();
+        }
+
+        public void ConvertTo(HtmlNode node, TextWriter outText)
+        {
+            string html;
+            switch (node.NodeType)
+            {
+                case HtmlNodeType.Comment:
+                    // don't output comments
+                    break;
+
+                case HtmlNodeType.Document:
+                    ConvertContentTo(node, outText);
+                    break;
+
+                case HtmlNodeType.Text:
+                    // script and style must not be output
+                    string parentName = node.ParentNode.Name;
+                    if ((parentName == "script") || (parentName == "style"))
+                        break;
+
+                    // get text
+                    html = ((HtmlTextNode)node).Text;
+
+                    // is it in fact a special closing node output as text?
+                    if (HtmlNode.IsOverlappedClosingElement(html))
+                        break;
+
+                    // check the text is meaningful and not a bunch of whitespaces
+                    if (html.Trim().Length > 0)
+                    {
+                        outText.Write(HtmlEntity.DeEntitize(html));
+                    }
+                    break;
+
+                case HtmlNodeType.Element:
+                    switch (node.Name)
+                    {
+                        case "p":
+                            // treat paragraphs as crlf
+                            outText.Write("\r\n");
+                            break;
+                    }
+
+                    if (node.HasChildNodes)
+                    {
+                        ConvertContentTo(node, outText);
+                    }
+                    break;
+            }
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private void ConvertContentTo(HtmlNode node, TextWriter outText)
+        {
+            foreach (HtmlNode subnode in node.ChildNodes)
+            {
+                ConvertTo(subnode, outText);
+            }
+        }
+        public static void Main()
+        {
+            HtmlToText obj = new HtmlToText();
+            string content = obj.Convert("http://msdn.microsoft.com/en-us/library/windows/hardware/hh162931.aspx");
+            System.IO.StreamWriter s = new StreamWriter("c:\\test.txt", false);
+            s.WriteLine(content);
+        }
+
+        #endregion
+    }
+}
